b54560d4743d89b5d5b7c8d39be2e1c4f8a786e1
Rehema S. Shungu <rshungu08@gmail.com>
2025-03-03 13:38:16 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,38 @@
+/*
+=================================================================================
+Create Database and Schemas
+=================================================================================
+Script Purpose: 
+  This script is used to drop and recreate the "DataWarehouse" database along with the creation of necessary schemas for data processing layers:
+  Bronze (raw data), Silver (cleaned/processed data), and Gold (aggregated/final data)
+
+WARNING:
+  This script will DROP the "DataWarehouse" database if it already exists, causing permanent deletion of all data within it. Use this script with caution.
+*/
+
+-- Drop and recreate the "DataWarehouse" database
+DROP DATABASE IF EXISTS "DataWarehouse";
+
+-- Create the "DataWarehouse" database with specified configurations
+CREATE DATABASE "DataWarehouse"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+USE DataWarehouse;
+
+
+-- Create Schemas for various layers of data processing
+CREATE SCHEMA IF NOT EXISTS bronze;
+
+CREATE SCHEMA IF NOT EXISTS gold;
+
+CREATE SCHEMA IF NOT EXISTS silver;
+
 
